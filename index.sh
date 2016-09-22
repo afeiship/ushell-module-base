5e2871321c73899601cddf829271f8744522ca9b
feizheng <1290657123@qq.com>
2016-09-22 23:37:51 +0800

    update base

--- index.sh
+++ index.sh
@@ -1,6 +1,12 @@
 #!/usr/bin/env bash
 ROOT_PATH=$(pwd);
 
-alias usm-base-reload="source ${ROOT_PATH}/index.sh";
 source $ROOT_PATH/cd.sh;
 source $ROOT_PATH/chmod.sh;
+alias usm-base-reload="source ${ROOT_PATH}/index.sh";
+alias ll='ls -al';
+alias cwd="pwd | tr -d '\n' | pbcopy";
+alias su-root='sudo su root';
+alias cls='clear';
+alias g-remove='rm -rf * .* *.*';
+alias g-cp='rsync -av --progress';
