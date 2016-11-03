6f9470029e35be1849426212b86c9721df9e7521
feizheng <1290657123@qq.com>
2016-11-03 19:00:34 +0800

    bugfix

--- index.sh
+++ index.sh
@@ -13,7 +13,7 @@ alias su-root='sudo su root';
 alias cls='clear';
 alias g-remove='rm -rf * .* *.*';
 alias g-cp='rsync -av --progress';
-alias rm-usm='rm -rf $BASH_BASE_PATH/ushell_modules/ushell-module-'
+alias rm-usm='rm -rf $BASH_BASE_PATH/ushell_modules'
 alias rm-usm-md='rm -rf $BASH_BASE_PATH/ushell_modules/ushell-module-$0';
 
 unset ROOT_PATH;
