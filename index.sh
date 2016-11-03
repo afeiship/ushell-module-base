0085f61b10dc844e4ce807ccd8b37721366fc8f8
feizheng <1290657123@qq.com>
2016-11-03 19:37:48 +0800

    add mkcd

--- index.sh
+++ index.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env bash
 ## path:
 ROOT_PATH=$(dirname $BASH_SOURCE);
+source $ROOT_PATH/function.sh;
 
 ## base:
 source $ROOT_PATH/cd.sh;
@@ -13,9 +14,12 @@ alias su-root='sudo su root';
 alias cls='clear';
 alias g-remove='rm -rf * .* *.*';
 alias g-cp='rsync -av --progress';
+alias mkcd='_(){ mkdir $1; cd $1; }; _';
+
 
 ## usm base modules:
 alias rm-usm='rm -rf $BASH_BASE_PATH/ushell_modules'
 alias rm-usm-md='rm -rf $BASH_BASE_PATH/ushell_modules/ushell-module-$1';
+
 
 unset ROOT_PATH;
