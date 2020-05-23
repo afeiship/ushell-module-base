510b81013683144db129ce701cb4a76b95c034c3
afeiship <1290657123@qq.com>
2020-05-23 09:57:57 +0800

    fix: remove del

--- index.sh
+++ index.sh
@@ -22,22 +22,12 @@ alias backup='cd ~/github/macos-backup/ && gg "feat(daily): automatic backup mac
 ## aliases:
 alias ll='ls -al';
 alias cwd="pwd | tr -d '\n' | pbcopy";
-alias su-root='sudo su root';
 alias cls='clear';
-alias g-remove='rm -rf * .* *.*';
-alias g-cp='rsync -av --progress';
-alias mkcd='_(){ mkdir $1; cd $1; }; _';
-alias rmf='rm -rf';
 alias size='du -hs *';
 
 # open
 alias o="open ."
 alias op="open "
-alias opp="open https://github.com/afeiship/$BASE_NAME"
 
-
-## usm base modules:
-alias usm-remove-all='rm -rf $BASH_BASE_PATH/ushell_modules'
-alias usm-remove-item='_(){ rm -rf $BASH_BASE_PATH/ushell_modules/ushell-module-$1; }; _';
 
 unset ROOT_PATH;
