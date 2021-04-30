e586fecede60853c321b2126a5b5ffdee69953c5
afeiship <1290657123@qq.com>
2021-04-30 09:54:14 +0800

    feat: update shorty

--- modules/shorty.sh
+++ modules/shorty.sh
@@ -1,13 +1,14 @@
 #!/usr/bin/env bash
 
-# c in ushell-module-vscode
-# alias c="code .";
+# `c` in ushell-module-vscode
 
 # npm
 alias b="npm run build";
 alias i="npm i";
 alias s="npm run start";
 alias v="npm run dev";
+
+# `y` in ushell-module-yarn
 
 # open/exit
 alias o="open .";
