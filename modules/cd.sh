1163472c546a4e2daec872e35ead96964bfb8e2c
afeiship <1290657123@qq.com>
2022-04-27 06:30:46 +0800

    feat: add base dir

--- modules/cd.sh
+++ modules/cd.sh
@@ -3,3 +3,13 @@
 alias ..='cd ..';
 alias ...='cd ../..';
 alias c-='cd -';
+
+
+# base dir
+alias notes="cd ~/aric-notes";
+alias server="cd ~/aric-server";
+alias lwb="cd ~/aric-lwb";
+alias spider="cd ~/aric-spider";
+alias business="cd ~/aric-business";
+alias gem="cd ~/aric-gem";
+alias pypi="cd ~/aric-pypi";
