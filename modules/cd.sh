1aeb6eef700c9b82d86e3e1f2434b4bc78051392
afeiship <1290657123@qq.com>
2022-04-30 10:22:31 +0800

    Release 1.0.19

--- modules/cd.sh
+++ modules/cd.sh
@@ -13,3 +13,5 @@ alias spider="cd ~/aric-spider";
 alias business="cd ~/aric-business";
 alias gem="cd ~/aric-gem";
 alias pypi="cd ~/aric-pypi";
+alias alo7="cd ~/alo7i";
+alias saybot="cd ~/saybot";
