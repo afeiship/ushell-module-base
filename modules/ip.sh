bc274cdb8edaa3df23d2a7b466ca300e5c57f1ae
afeiship <1290657123@qq.com>
2022-10-26 13:29:12 +0800

    fix: only server will get ip

--- modules/ip.sh
+++ modules/ip.sh
@@ -1,10 +1,11 @@
 #!/usr/bin/env bash
 
-# server ip
-export SERVER_IP=`wget -qO- ipecho.net/plain -T 2`;
-# for macos
 if [[ $(uname) == Darwin ]]; then
     export SERVER_IP='127.0.0.1';
+else
+    export SERVER_IP=`wget -qO- ipecho.net/plain -T 2`;
 fi
+
+
 
 
