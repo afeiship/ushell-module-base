d0962447d24c6b9f988b9f6ea6e47415e883b524
afeiship <1290657123@qq.com>
2025-01-06 11:27:20 +0800

    feat: upt ins 2 args

--- modules/_ins.sh
+++ modules/_ins.sh
@@ -1,5 +1,15 @@
 #!/usr/bin/env bash
 
 function ins() {
-  cp "$1" /usr/local/bin/
+  if [ $# -eq 1 ]; then
+    # 只有一个参数，保持现有逻辑
+    cp "$1" /usr/local/bin/
+  elif [ $# -eq 2 ]; then
+    # 如果有两个参数，将 $1 复制到 /usr/local/bin/$2
+    cp "$1" "/usr/local/bin/$2"
+  else
+    # 参数错误提示
+    echo "Usage: ins <source> [destination]"
+    return 1
+  fi
 }
