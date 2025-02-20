1ad5b928799d0385331c575a5726e02c9747c449
afeiship <1290657123@qq.com>
2025-02-20 09:56:30 +0800

    fix: add missing permission

--- modules/_ins.sh
+++ modules/_ins.sh
@@ -4,9 +4,13 @@ function ins() {
   if [ $# -eq 1 ]; then
     # 只有一个参数，保持现有逻辑
     cp "$1" /usr/local/bin/
+    # 给权限
+    chmod +x "/usr/local/bin/$1"
   elif [ $# -eq 2 ]; then
     # 如果有两个参数，将 $1 复制到 /usr/local/bin/$2
     cp "$1" "/usr/local/bin/$2"
+    # 给权限
+    chmod +x "/usr/local/bin/$2"
   else
     # 参数错误提示
     echo "Usage: ins <source> [destination]"
